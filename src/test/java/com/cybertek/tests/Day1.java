c8cfd5454b50bd2dc0241620fb573b96bd665e27
CybertekSchool <github@cybertekschool.com>
2020-07-19 15:46:40 -0400

    day1 etsy

--- src/test/java/com/cybertek/tests/Day1.java
+++ src/test/java/com/cybertek/tests/Day1.java
@@ -135,6 +135,47 @@ public class Day1 {
 
     }
 
+    @Test
+    public void realPhoneTest() throws MalformedURLException, InterruptedException {
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+        desiredCapabilities.setCapability(MobileCapabilityType.VERSION,"9.0");
+        //we used real device, i get this UUID number from terminal with typing "adb devices"
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"9887fc474f4f315a43");
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
+        //to specfiy app for testing
+        //it can be on your computer or somewhere in cloud
+        desiredCapabilities.setCapability("app","https://cybertek-appium.s3.amazonaws.com/etsy.apk");
+        driver = new AppiumDriver<>(new URL("http://localhost:4723/wd/hub"),desiredCapabilities);
+
+        Thread.sleep(5000);
+
+        MobileElement you = driver.findElement(MobileBy.AccessibilityId("You tab, 4 of 5"));
+        you.click();
+        Thread.sleep(2000);
+
+        MobileElement settings = driver.findElement(By.xpath("//*[@text='Settings']"));
+        settings.click();
+        Thread.sleep(2000);
+
+        MobileElement checkbox = driver.findElement(By.id("com.etsy.android:id/settings_checkbox"));
+        checkbox.click();
+        Thread.sleep(3000);
+
+        //verify after click the checkbox it is not selected
+        Assert.assertFalse(driver.findElement(By.id("com.etsy.android:id/settings_checkbox")).isSelected());
+        Thread.sleep(1000);
+
+
+
+        driver.closeApp();
+
+    }
+
+
+
 
 
 
